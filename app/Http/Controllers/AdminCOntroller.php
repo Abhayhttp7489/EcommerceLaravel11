2a31e6708285bd870d1f39057d15108de3fefd06
Abhay <abhay.bdmu@gmail.com>
2025-01-31 17:46:46 +0530

    update

--- app/Http/Controllers/AdminCOntroller.php
+++ app/Http/Controllers/AdminCOntroller.php
@@ -10,4 +10,10 @@ class AdminCOntroller extends Controller
     {
         return view('admin.index');
     }
+
+    public function brand()
+    {
+        $brands = Brand::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.brands', compact('brands'));
+    }
 }
--- app/Http/Middleware/AuthAdmin.php
+++ app/Http/Middleware/AuthAdmin.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthAdmin
@@ -12,25 +13,21 @@ class AuthAdmin
     /**
      * Handle an incoming request.
      *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if(Auth::check())
-        {
-            if(Auth::user()->utype === 'ADM')
-            {
+        if (Auth::check()) {
+            if (Auth::user()->utype === 'ADM') {
                 return $next($request);
+            } else {
+                Session::flush();
+                return redirect()->route('login')->with('error', 'Access denied. Admin privileges required.');
             }
-            else
-            {
-              session::flush();
-              return redirect()->route('login');
-            }
-        }
-        else{
-            return redirect()->route('login');
         }
         
+        return redirect()->route('login')->with('error', 'Please login to continue.');
     }
 }
